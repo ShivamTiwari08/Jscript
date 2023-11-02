972bec62f520f237444c91d6cb468eb8c275d809
Shivam Tiwari <145989727+ShivamTiwari08@users.noreply.github.com>
2023-11-02 18:35:35 +0000

    var and DT

---
+++ Basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS Code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3
+    + 
+     3) // code readability should be high
+console.log("Shivam")
+
+let name = "Shivam"
+let age = 18
+let isloggedIn = false
+let state = null
+let type;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+//object
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
+
+
